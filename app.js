6fcc18642fbee58f09c00432f85df0d9d3d34784
Mario Garcia <mgarciac444@gmail.com>
2023-03-21 00:13:44 +0100

    Juego con movimiento y turnos

--- app.js
+++ app.js
@@ -1,66 +1,124 @@
 let main = document.getElementsByTagName('main')[0];
-main.classList.add('container');
 
 // Coloco el numero de filas y columnas tipo tablero de ajedrez
 let nFilas = 8;
 let nColumnas = 8;
 
-let div, objetivo, j1, j2;
+let objetivo, j1, j2, turno;
+let divJuego = document.createElement("div");
+divJuego.classList.add('container');
+
+let divInfo = document.createElement("div");
+divInfo.classList.add('container2');
+
+let reiniciar = document.createElement("button");
+reiniciar.textContent = "Reiniciar"
+reiniciar.setAttribute("type", "button");
+reiniciar.setAttribute("onclick", "inicio()");
+reiniciar.classList.add('reinicio');
+
+divInfo.appendChild(reiniciar);
+main.appendChild(divJuego);
+main.appendChild(divInfo);
 
 // Creamos el inicio del tablero
 document.addEventListener('load', inicio());
+reiniciar.setAttribute("onclick", "colocarFichas()");
 
-/**
- * Funcion que inicia el tablero
- */
-function inicio(){
+// Funcion que inicia el tablero
+function inicio() {
+    let div0;
+    let div;
     // Bucles para crear filas y columas
-    for (let i = 0; i < nFilas; i++){
-        div0 = document.createElement('div'); 
-        for(let j = 0;  j < nColumnas; j++){
-            div = document.createElement('div'); 
+    for (let i = 0; i < nFilas; i++) {
+        div0 = document.createElement('div');
+        for (let j = 0; j < nColumnas; j++) {
+            div = document.createElement('div');
             div.classList.add('card');
-            div.setAttribute('id', `f${i}c${j}`);
-
+            div.setAttribute('id', `c${i}f${j}`);
             div0.appendChild(div);
         }
-        main.appendChild(div0);
+        divJuego.appendChild(div0);
     }
     colocarFichas();
 }
 
-/**
- * Function que coloca las casillas inicialmente (ALEATORIO TODO)
- */
-function colocarFichas(){
-    
-    let fila;
-    let columna;
-    let posicion;
-    fila=posicionRandom(nFilas);
-    columna=posicionRandom(nColumnas);
-    posicion=`f${fila}c${columna}`;
+// Function que coloca las casillas inicialmente (ALEATORIO TODO)
+function colocarFichas() {
+    // El turno empieza para j1
+    turno = false;
+    // Borrar casillas partida anterior si las hay
+    for (let i = 0; i < nFilas; i++) {
+        for (let j = 0; j < nColumnas; j++) {
+            let dv = document.getElementById(`c${i}f${j}`);
+            dv.classList.remove("objetivo");
+            dv.classList.remove("j1");
+            dv.classList.remove("j2");
+        }
+    }
+    // Generar las 3 posiciones aleatorias sin repetirse
+    let posicionesFin = noRepiten();
 
-    objetivo = document.getElementById(posicion);
+    // Guardar posiciones y ponerles una case
+    objetivo = document.getElementById(posicionesFin[0]);
     objetivo.classList.add('objetivo');
-
-    j1 = document.getElementById('f1c3');
+    j1 = document.getElementById(posicionesFin[1]);
     j1.classList.add('j1');
-
-    j2 = document.getElementById('f3c0');
+    j2 = document.getElementById(posicionesFin[2]);
     j2.classList.add('j2');
 }
 
 //Hacemos que la genaracion inicial de las casillas sea aleatoria
-function posicionRandom(cantidad){
+/**
+ * @returns {Array} posiciones;
+ */
+function noRepiten() {
+    let posiciones = [];
+    let cont = 0;
+    let nroObjetos = 3;
+    let esta;
+    let fila;
+    let columna;
     let posicion;
-    posicion=getRandomInt(cantidad+1);
+    do {
+        esta = false;
+        fila = posicionRandom(nFilas);
+        columna = posicionRandom(nColumnas);
+        posicion = `c${columna}f${fila}`;
+        if (cont == 0) {
+            posiciones.push(posicion);
+            cont++;
+        } else {
+            for (let i = 0; i < posiciones.length; i++) {
+                if (posicion == posiciones[i]) {
+                    esta = true;
+                }
+            }
+            if (esta == false) {
+                posiciones.push(posicion);
+                cont++;
+            }
+        }
+    } while (cont < nroObjetos);
+    return posiciones;
+}
+/**
+ * @param {Int} cantidad
+ * @returns {Int} posicion
+ */
+function posicionRandom(cantidad) {
+    let posicion;
+    posicion = getRandomInt(cantidad);
     return posicion
 }
 
+/**
+ * @param {Int} max
+ * @returns {Int} Math.floor()
+ */
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 // Creamos el evento de escucha de tecaldo
 document.addEventListener('keydown', mover);
 
@@ -71,13 +129,160 @@ document.addEventListener('keydown', mover);
  * 
  * @param {Object} event - Informacion sobre el evento que se ha ejecutado  
  */
-function mover(event){
-    console.log(event);
-    /**
-     * Identificamos con el objeto KeyboardEvent cual es la tecla que estoy pulsando.
-     * -    key
-     * -    code
-     */
+function mover(event) {
+    let actual;
+    let columnaNueva;
+    let filaNueva;
+    if (!turno) {
+        switch (event.keyCode) {
+            // Arriba
+            case 38:
+                //Borrar la posicion anterior 
+                j1.classList.remove("j1");
+
+                actual = j1.id;
+                columnaNueva = actual.charAt(1);
+                filaNueva = actual.charAt(3);
+                if (filaNueva == 0) {
+                    filaNueva = 7;
+                }
+                else {
+                    filaNueva--;
+                }
+                j1 = document.getElementById(`c${columnaNueva}f${filaNueva}`);
+                j1.classList.add('j1');
+                break;
+            // Abajo
+            case 40:
+                //Borrar la posicion anterior 
+                j1.classList.remove("j1");
+
+                actual = j1.id;
+                columnaNueva = actual.charAt(1);
+                filaNueva = actual.charAt(3);
+                if (filaNueva == 7) {
+                    filaNueva = 0;
+                }
+                else {
+                    filaNueva++;
+                }
+                j1 = document.getElementById(`c${columnaNueva}f${filaNueva}`);
+                j1.classList.add('j1');
+                break;
+            // Izq
+            case 37:
+                //Borrar la posicion anterior 
+                j1.classList.remove("j1");
+
+                actual = j1.id;
+                columnaNueva = actual.charAt(1);
+                filaNueva = actual.charAt(3);
+                if (columnaNueva == 0) {
+                    columnaNueva = 7;
+                }
+                else {
+                    columnaNueva--;
+                }
+                j1 = document.getElementById(`c${columnaNueva}f${filaNueva}`);
+                j1.classList.add('j1');
+                break;
+            // Der
+            case 39:
+                //Borrar la posicion anterior 
+                j1.classList.remove("j1");
+
+                actual = j1.id;
+                columnaNueva = actual.charAt(1);
+                filaNueva = actual.charAt(3);
+                if (columnaNueva == 7) {
+                    columnaNueva = 0;
+                }
+                else {
+                    columnaNueva++;
+                }
+                j1 = document.getElementById(`c${columnaNueva}f${filaNueva}`);
+                j1.classList.add('j1');
+                break;
+            default:
+                turno = !turno;
+                break;
+        }
+    } else {
+        switch (event.keyCode) {
+            // Arriba
+            case 87:
+                //Borrar la posicion anterior 
+                j2.classList.remove("j2");
+
+                actual = j2.id;
+                columnaNueva = actual.charAt(1);
+                filaNueva = actual.charAt(3);
+                if (filaNueva == 0) {
+                    filaNueva = 7;
+                }
+                else {
+                    filaNueva--;
+                }
+                j2 = document.getElementById(`c${columnaNueva}f${filaNueva}`);
+                j2.classList.add('j2');
+                break;
+            // Abajo
+            case 83:
+                //Borrar la posicion anterior 
+                j2.classList.remove("j2");
+
+                actual = j2.id;
+                columnaNueva = actual.charAt(1);
+                filaNueva = actual.charAt(3);
+                if (filaNueva == 7) {
+                    filaNueva = 0;
+                }
+                else {
+                    filaNueva++;
+                }
+                j2 = document.getElementById(`c${columnaNueva}f${filaNueva}`);
+                j2.classList.add('j2');
+                break;
+            // Izq
+            case 65:
+                //Borrar la posicion anterior 
+                j2.classList.remove("j2");
+
+                actual = j2.id;
+                columnaNueva = actual.charAt(1);
+                filaNueva = actual.charAt(3);
+                if (columnaNueva == 0) {
+                    columnaNueva = 7;
+                }
+                else {
+                    columnaNueva--;
+                }
+                j2 = document.getElementById(`c${columnaNueva}f${filaNueva}`);
+                j2.classList.add('j2');
+                break;
+            // Der
+            case 68:
+                //Borrar la posicion anterior 
+                j2.classList.remove("j2");
+
+                actual = j2.id;
+                columnaNueva = actual.charAt(1);
+                filaNueva = actual.charAt(3);
+                if (columnaNueva == 7) {
+                    columnaNueva = 0;
+                }
+                else {
+                    columnaNueva++;
+                }
+                j2 = document.getElementById(`c${columnaNueva}f${filaNueva}`);
+                j2.classList.add('j2');
+                break;
+            default:
+                turno = !turno;
+                break;
+        }
+    }
+    turno = !turno;
 }
 
 /**
